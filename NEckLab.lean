bb55d941aa28c9dacd769482ff5c90f519eaa473
cr4bbz <voigtjulianleon@gmail.com>
2026-09-16 18:54:50 +0200

    Expose Gates 3-10 research modules

--- NEckLab.lean
+++ NEckLab.lean
@@ -1,3 +1,7 @@
 import NEckLab.Foundations
 import NEckLab.RegularPolygon
+import NEckLab.LimitsAndDimension
+import NEckLab.Predecessor
+import NEckLab.FractalCandidate
+import NEckLab.TwoAxis
 import NEckLab.Visualization
