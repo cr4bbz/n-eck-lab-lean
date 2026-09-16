25c7b4ed14250e20a6b593950a2aa2ba95c1afd2
cr4bbz <voigtjulianleon@gmail.com>
2026-09-16 19:11:06 +0200

    Normalize Gate 3 cosine composition and limit value

--- NEckLab/LimitsAndDimension.lean
+++ NEckLab/LimitsAndDimension.lean
@@ -44,8 +44,9 @@ theorem tendsto_radialDefect_zero :
     simpa using
       (tendsto_const_nhds.mul tendsto_inverseScale_zero :
         Tendsto (fun n : ℕ => Real.pi * inverseScale n) atTop (𝓝 (Real.pi * 0)))
-  have hcos := (Real.continuous_cos.tendsto 0).comp harg
-  change Tendsto (fun n : ℕ => Real.cos (Real.pi * inverseScale n)) atTop (𝓝 1) at hcos
+  have hcos : Tendsto (fun n : ℕ => Real.cos (Real.pi * inverseScale n)) atTop (𝓝 1) := by
+    simpa only [Function.comp_def, Real.cos_zero] using
+      ((Real.continuous_cos.tendsto 0).comp harg)
   change Tendsto (fun n : ℕ => 1 - Real.cos (Real.pi * inverseScale n)) atTop (𝓝 0)
   simpa using
     (tendsto_const_nhds.sub hcos :
